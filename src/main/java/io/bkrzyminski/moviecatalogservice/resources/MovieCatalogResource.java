66b78ad62a6c72318192bb4f26f5e8d329fa662d
bkrzyminski <krzyminski.bartlomiej@gmail.com>
2019-12-29 21:01:35 +0100

    Added models, basic communication between other services through REST

--- src/main/java/io/bkrzyminski/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/io/bkrzyminski/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,21 +1,35 @@
 package io.bkrzyminski.moviecatalogservice.resources;
 
 import io.bkrzyminski.moviecatalogservice.models.CatalogItem;
+import io.bkrzyminski.moviecatalogservice.models.Movie;
+import io.bkrzyminski.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable String userId){
-        return Collections.singletonList(
-                new CatalogItem("Lord Of The Rings", "Movie about hobbits", 10)
+    public List<CatalogItem> getCatalog(@PathVariable String userId) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 87)
         );
+
+        return ratings.stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+                })
+                .collect(Collectors.toList());
     }
 }
